d15bc3ed5e60b1f8dffe56981bd544946125fb66
Joseph Son <sms0330@gmail.com>
2021-09-13 15:33:54 -0700

    Fixed show style

--- src/components/CoinShowPage.js
+++ src/components/CoinShowPage.js
@@ -34,21 +34,21 @@ class CoinShowPage extends Component {
     const { name, symbol, image, market_data } = this.state.coin;
     return (
       <main>
-        <div className="ui placeholder segment">
-          <div className="ui two column very relaxed stackable grid">
-            <div className="column">
+        <div className="ui segment">
+          <div className="ui two column very relaxed grid">
+            <div className="column" id="show-column">
               <div className="ui form">
                 <div className="field">
                   <label><img src={image.thumb} className='logo' alt='image'/>{name.toUpperCase()}{`(${symbol.toUpperCase()})`}</label>
                 </div>
-                <hr className="show line" />
-                <p>CURRENT PRICE: ${market_data.current_price.cad}</p>
-                <p>MARKET CAP: {market_data.market_cap.cad}</p>
-                <p>LOW 24H: {market_data.low_24h.cad}</p>
-                <p>HIGH 24H: {market_data.high_24h.cad}</p>
-                <p>CIRCULATING SUPPLY: {market_data.circulating_supply}</p>
-                <p>TOTAL SUPPLY: {market_data.total_supply}</p>
-                <p>MARKET CAP RANK: {market_data.market_cap_rank}</p>
+                <hr className="show-line" />
+                <p><strong>CURRENT PRICE:</strong> ${market_data.current_price.cad}</p>
+                <p><strong>MARKET CAP:</strong> {market_data.market_cap.cad}</p>
+                <p><strong>LOW 24H:</strong> {market_data.low_24h.cad}</p>
+                <p><strong>HIGH 24H:</strong> {market_data.high_24h.cad}</p>
+                <p><strong>CIRCULATING SUPPLY:</strong> {market_data.circulating_supply}</p>
+                <p><strong>TOTAL SUPPLY:</strong> {market_data.total_supply}</p>
+                <p><strong>MARKET CAP RANK:</strong> {market_data.market_cap_rank}</p>
                 <div className="favourite">
                   <button>add to favourite</button>
                 </div>
@@ -57,8 +57,10 @@ class CoinShowPage extends Component {
             <div className="column">
               <form>
                 <div className="two column row">
+                  <br/>
                   <button className="ui orange basic button">BUY</button>
                   <button className="ui orange basic button">SELL</button>
+                  <div/><br/>
                   <div className="ui dropdown item">
                     Select Trade Currency <i className="dropdown icon"></i>
                     <div className="menu">
@@ -66,7 +68,8 @@ class CoinShowPage extends Component {
                         <a className="item">ETEHEREUM</a>
                         <a className="item">XRP</a>
                     </div>
-                  </div>
+                  </div><br/>
+                  <br/>
                   <div className="ui dropdown item">
                     Amount <i className="dropdown icon"></i>
                     <div className="menu">
@@ -74,13 +77,12 @@ class CoinShowPage extends Component {
                         <a className="item">2</a>
                         <a className="item">3</a>
                     </div>
-                  </div>
+                  </div><br/><br/>
                   <button className="ui orange button">SUBMIT</button>
                 </div>
               </form>
             </div>
           </div>
-          <div className="ui vertical divider"></div>
         </div>
       </main>
     );
