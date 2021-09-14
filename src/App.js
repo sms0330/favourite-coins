0f5266d84d4930f7b7026e81201dfa063b6c4aeb
Joseph Son <sms0330@gmail.com>
2021-09-14 13:34:54 -0700

    Fixed favourite coins

--- src/App.js
+++ src/App.js
@@ -4,17 +4,57 @@ import CoinIndexPage from './components/CoinIndexPage';
 import FavouritePage from './components/FavouritePage';
 import Spinner from "./components/Spinner";
 import CoinShowPage from './components/CoinShowPage';
+
+// let lastId = savedFavourites.length-1
+let saveData = JSON.parse(localStorage.getItem("favourites"));
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false, 
-      favourites: ['a']
+      favourites: saveData ? saveData : [],
+      images: []
     }
   }
 
-  liftStateUp = (data) =>{
-    this.setState({ favourites: [ data, ...this.state.favourites ]  })
+  // handleSave = () => {
+  //   localStorage.setItem("favourites", JSON.stringify(this.state.favourites))
+  // }
+
+  componentDidMount() {
+    const savedFavourites = JSON.parse(localStorage.getItem("favourites"));
+    if (savedFavourites !== null) {
+      this.setState(savedFavourites)
+    }
+    console.log('Component did mount')
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.favourites !== this.state.favourites) {
+      localStorage.setItem('favourites', JSON.stringify(this.state.favourites))
+        // this.setState( )
+        console.log('changed')
+    }
+  }
+
+  childCallback(e, value, image) {
+    // value passed from child
+    if (!this.state.favourites.includes(value)) {
+    e.preventDefault();
+    console.log(value);
+    console.log(this.state)
+    this.setState({favourites: [...this.state.favourites, value]});
+    this.setState({images: [...this.state.images, image]});
+    }
+  }
+
+  remove(i) {
+    this.state.favourites.splice(i,1);
+    this.setState({favourites: [...this.state.favourites]})
+    this.state.images.splice(i,1);
+    this.setState({images: [...this.state.images]})
+    console.log(this.state.favourites)
+
   }
 
   render() {
@@ -24,14 +64,13 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
-          <FavouritePage clickedCoins={this.state.favourites}/>
+          <FavouritePage favourites={this.state.favourites} images={this.state.images} remove={this.remove.bind(this)}/>
           <CoinIndexPage />
             <Switch>
               <Route
                 exact
                 path="/coins/:id"
-                // component={CoinShowPage}
-                render={(props) => <CoinShowPage {...props} clickedCoins={this.state.favourites} />}
+                render={(props) => <CoinShowPage {...props} passToParent={this.childCallback.bind(this)} />}
 
               />
             </Switch>
--- src/components/FavouritePage.js
+++ src/components/FavouritePage.js
@@ -1,101 +1,82 @@
 import React, { Component } from 'react';
-import Bitcoin from './assets/img/Bitcoin.png';
-import BitcoinCash from './assets/img/BitcoinCash.png';
-import Ethereum from './assets/img/Ethereum.png';
-import Litecoin from './assets/img/Litecoin.png';
-import XRP from './assets/img/XRP.png';
+import Bitcoin from './assets/img/Bitcoin.svg';
+import BitcoinCash from './assets/img/BCH.svg';
+import Ethereum from './assets/img/Ethereum.svg';
+import Litecoin from './assets/img/Litecoin-new.svg';
+import XRP from './assets/img/XRP.svg';
 import Spinner from './Spinner';
 
-const images = { btc: Bitcoin, bch: BitcoinCash, eth: Ethereum, xrp: XRP, ltc: Litecoin };
+const images = { 'BITCOIN': Bitcoin, 'BITCOIN CASH': BitcoinCash, 'ETHEREUM': Ethereum, 'XRP': XRP, 'LITECOIN': Litecoin }
 class FavouritePage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-      favourites: [...this.props.clickedCoins]
-    };
-  }
-
-  setFavourite(name) {
-    this.setState({ favourites: name });
-    this.props.liftStateUp(name);
-    console.log(this.state.favourites);
-  }
-
-  deleteFavourite(id) {
-    this.setState(state => {
-      return {
-        favourites: state.favourites.filter(f => f.id !== id),
-      };
-    });
-  }
-
-  componentDidMount() {
-    this.setState(state => {
-      return {
-        favourites: state.favourites
-      };
-    });
-  }
-
-//   componentDidUpdate(prevProps) {
-//     if (this.props.clickedCoins !== prevProps.props.clickedCoins) {
-//       this.setState(state => {
-//         return {
-//           favourites: state.favourites,
-//         };
-//       });
-//     }
-//   }
-
-  render() {
-    if (!this.state.favourites) {
-      return <Spinner />;
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false, favourites: []
+        };
     }
-    const set = new Set(this.state.favourites);
-    const uniqueCoins = [...set];
-    console.log(uniqueCoins)
-    return (
-      <div className="head">
-        <h1 className="head title">Favourite Coins</h1>
-        <button onClick={() => console.log(this.props.clickedCoins)}>Button</button>
-        {/* //this.props.clickedCoins!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */}
 
-        {uniqueCoins.map((f, i) => {
-          return (
-            <div key={i} id={f.id}>
-              <h1>{f}</h1>
+    // setFavourite(name) {
+    //     this.setState({ favourites: name });
+    //     this.props.liftStateUp(name);
+    //     console.log(this.state.favourites);
+    // }
+
+    deleteFavourite(id) {
+        this.setState((state) => {
+            return {
+                favourites: state.favourites.filter(f => f.id !== id)
+            }
+        })
+    }
+
+    // componentDidMount() {
+    //     // this.setState((state) => {
+    //     // })
+    //     console.log('hi')
+    // }
+    
+    //   componentDidUpdate(prevProps, prevState) {
+    //     if (prevProps.clickedCoins !== this.props.clickedCoins) {
+    //         // this.setState( )
+    //         console.log('changed')
+    //     }
+    //   }
+
+    render() {
+        if (!this.state.favourites) {
+            return <Spinner />;
+          }
+
+
+        return (
+            <div className='head'>
+                <h1 className="head title">
+                    Favourite Coins
+                </h1>
+                {/* <button onClick={() => console.log(this.props.favourites)}>Button</button> */}
+                {/* //this.props.favourites!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */}
+                
+                {this.props.favourites.map((el, i)=>{
+                    return (
+                        
+                        <button 
+                        key={i}
+                        // data-hover='remove' 
+                        onClick={() => this.props.remove(i)}
+                        className="faveButtons"
+                        >
+                            {/* { <span class="iconify" data-icon={`logos:${icons.el}`}></span>} */}
+                            <span id="favorite">
+                                <span style={{verticalAlign:"middle"}}><img src={images[el]} alt="logo"/></span>
+                                <span style={{verticalAlign:"middle"}}> {el}</span>
+                            </span>
+                        </button>
+                      
+                    )
+                })} 
             </div>
-          );
-        })}
-
-        {/* <div className='favourite_list'>
-                    { this.state.favourites.length === 0? 
-                        <small className="string">
-                            No Favourite Coins Added
-                        </small>
-                    :
-
-                    this.state.favourites.map((f) => {
-                        return (
-                            
-                            <button className='buttons' 
-                                data-hover='remove' 
-                                key={f} id={f} 
-                                onClick={() => this.deleteFavourite(f)}
-                                    >
-                                <img src={images[f]} 
-                                    alt='hi' 
-                                    className='logo'
-                                    /> <p> {f} </p>
-                            </button>
-                        )
-                    })
-                }
-                </div> */}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default FavouritePage;
